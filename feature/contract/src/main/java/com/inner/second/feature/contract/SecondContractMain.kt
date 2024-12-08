c558c691544b35f9a040bcf46fcb9ecec487b08f
inner <khsbs001@gmail.com>
2024-12-08 21:50:08 +0900

    end:

--- feature/contract/src/main/java/com/inner/second/feature/contract/SecondContractMain.kt
+++ feature/contract/src/main/java/com/inner/second/feature/contract/SecondContractMain.kt
@@ -1,5 +1,6 @@
 package com.inner.second.feature.contract
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,8 +12,13 @@ import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,23 +32,42 @@ import com.inner.second.core.designsystem.theme.Background
 import com.inner.second.core.designsystem.theme.Green
 import com.inner.second.core.designsystem.theme.Purple
 import com.inner.second.core.model.ContractType
+import kotlinx.coroutines.launch
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun SecondContractMainRoute(
     contractViewModel: NewContractViewModel = hiltViewModel(),
     navigateToGetInfo: (ContractType) -> Unit,
 ) {
-    SecondContractMainScreen(
-        onButtonClick = { contractType ->
-            contractViewModel.onContractTypeChange(contractType)
-            navigateToGetInfo(contractType)
+    val scope = rememberCoroutineScope()
+    val snackBarHostState = remember {
+        SnackbarHostState()
+    }
+    
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState)
         }
-    )
+    ) {
+        SecondContractMainScreen(
+            onButtonClick = { contractType ->
+                contractViewModel.onContractTypeChange(contractType)
+                navigateToGetInfo(contractType)
+            },
+            showSnackBar = {
+                scope.launch {
+                    snackBarHostState.showSnackbar(it)
+                }
+            }
+        )
+    }
 }
 
 @Composable
 fun SecondContractMainScreen(
-    onButtonClick: (ContractType) -> Unit
+    onButtonClick: (ContractType) -> Unit,
+    showSnackBar: (String) -> Unit,
 ) {
     Column(
         modifier = Modifier
@@ -55,14 +80,16 @@ fun SecondContractMainScreen(
             onBackButtonClick = { /* TODO : implement back button */ }
         )
         SecondContractMainContent(
-            onButtonClick = onButtonClick
+            onButtonClick = onButtonClick,
+            showSnackBar = showSnackBar,
         )
     }
 }
 
 @Composable
 private fun SecondContractMainContent(
-    onButtonClick: (ContractType) -> Unit
+    onButtonClick: (ContractType) -> Unit,
+    showSnackBar: (String) -> Unit
 ) {
     Column(
         modifier = Modifier
@@ -77,10 +104,10 @@ private fun SecondContractMainContent(
         Spacer(modifier = Modifier.height(12.dp))
         SecondButton(
             titleIconRes = R.drawable.ic_document_outlined,
-            text = "기존 계약서 스캔하기"
+            text = "기존 계약서 스캔하기",
+            onClick = { showSnackBar("슬픈 사연으로 인하여 구현하지 못하게 되었습니다...") }
         )
         Spacer(modifier = Modifier.height(12.dp))
-
         Text(
             text = "세컨드에서 제공하는 양식",
             fontWeight = FontWeight.Bold,
@@ -100,6 +127,7 @@ private fun SecondContractMainContent(
             trailingIconRes = R.drawable.ic_chevron_right,
             text = "레슨 · 과외 표준계약서",
             titleIconTintColor = Purple,
+            onClick = { showSnackBar("추후 오픈 예정입니다.") }
         )
     }
 }
@@ -108,6 +136,7 @@ private fun SecondContractMainContent(
 @Composable
 private fun SecondContractScreenPreview() {
     SecondContractMainScreen(
-        onButtonClick = {}
+        onButtonClick = {},
+        showSnackBar = {},
     )
 }
